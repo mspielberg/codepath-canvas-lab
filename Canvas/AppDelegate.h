f29575b63beb2a349a2cdaa8862cc9b298df4f0a
Miles Spielberg <miles@box.com>
2015-02-25 19:10:56 -0800

    Initial Commit

---
+++ Canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Canvas
+//
+//  Created by Miles Spielberg on 2/25/15.
+//  Copyright (c) 2015 OrionNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Canvas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Canvas
+//
+//  Created by Miles Spielberg on 2/25/15.
+//  Copyright (c) 2015 OrionNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
